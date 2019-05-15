dd6b556d593d90039ad8c878962a755d535622cc
petterastedt <petter.astedt@gmail.com>
2019-05-15 12:10:33 +0200

    Tweaks on timer component

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -3,18 +3,15 @@ import React, { useState, useEffect, useRef } from 'react'
 export default function Timer() {
   const [timerIsStarted, setTimerIsStarted] = useState(false)
   const [timerIsHidden, setTimerIsHidden] = useState(true)
-  const countdownSeconds = useRef(900) // Number of seconds to count down, default 900 (15min)
+  const countdownSeconds = useRef(3) // Number of seconds to count down, default 900 (15min)
   const [secMin, setSecMin] = useState({
     min: Math.floor(countdownSeconds.current / 60),
     sec: '00'
   })
   useEffect(() => {
-    if (countdownSeconds.current < 0) {
-      const alarm = new Audio(require('../audio/alarm.mp3'))
-      alarm.play()
-    }
     if (timerIsStarted & countdownSeconds.current >= 0) {
     const timeout = setTimeout(() => {
+      let alarm = new Audio(require('../audio/alarm.mp3'))
       let minutes = Math.floor(countdownSeconds.current / 60)
       let seconds = countdownSeconds.current - minutes * 60
       minutes = minutes < 10 ? "0" + minutes : minutes
@@ -24,6 +21,14 @@ export default function Timer() {
         min: minutes
       })
       countdownSeconds.current = countdownSeconds.current - 1
+      if (countdownSeconds.current < 0) {
+      alarm.play()
+      alarm.volume = 0.5
+      setTimeout(() => {
+        alarm.pause();
+        alarm.currentTime = 0;
+      }, 8000)
+      }
     }, 1000)
     return () => {
       clearTimeout(timeout)
